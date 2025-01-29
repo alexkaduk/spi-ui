d7e1d62cfdf0b0046a9f35d4754318e94c80e709
Alex <>
2025-01-29 13:46:14 +0200

    add storybook. add stories for Button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,12 +1,40 @@
 import React, { FC, PropsWithChildren } from "react";
+import cn from "classnames";
 import styles from "./button.module.scss";
 
-export const Button: FC<PropsWithChildren<Props>> = ({ children }) => {
-  return <button className={styles["button"]}>{children}</button>;
+/** Primary UI component for user interaction */
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? styles["button--primary"]
+    : styles["button--secondary"];
+  // cn('foo', { bar: true }); // => 'foo bar'
+  return (
+    <button
+      type="button"
+      className={cn(styles["button"], styles[`button--${size}`], mode)}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
 };
 
-export interface Props {
-  children: React.ReactNode;
-  className?: string;
-  fontSize?: string;
+export interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: "small" | "medium" | "large";
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
 }
